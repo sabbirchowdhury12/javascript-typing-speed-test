14cb6f4b2b7b0b354933613c7ad0e954804683a8
sabbir <sabbir@gmail.com>
2023-09-24 17:32:59 +0600

    display question

--- js/main.js
+++ js/main.js
@@ -1,1 +1,10 @@
-console.log("done");
+const question = document.getElementById("question");
+
+// display question
+fetch("/texts.json")
+  .then((res) => res.json())
+  .then((data) => {
+    const questionText = data[Math.floor(Math.random() * data.length)];
+    console.log(question);
+    question.textContent = questionText;
+  });
