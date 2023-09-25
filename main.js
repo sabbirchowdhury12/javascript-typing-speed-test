673de4d763980882ee30d29eb169dc2a74a83617
sabbir <sabbir@gmail.com>
2023-09-25 23:06:27 +0600

    add history and display history

--- main.js
+++ main.js
@@ -5,6 +5,7 @@ const showTime = document.getElementById("show-time");
 const display = document.getElementById("display");
 const result = document.getElementById("result");
 const modal = document.getElementById("modal-background");
+const histories = document.getElementById("histories");
 
 let questionText = "";
 let errorCount = 0;
@@ -18,6 +19,10 @@ fetch("/texts.json")
     questionText = data[Math.floor(Math.random() * data.length)];
     question.textContent = questionText;
   });
+
+function addHistory(questionText, timeTaken, errorCount) {
+  console.log("hi");
+}
 
 const typeController = (e) => {
   const newKey = e.key;
@@ -69,6 +74,14 @@ const endGame = () => {
       <button onclick="closeModal()">Close</button>
     `;
 
+  // Call addHistory with the appropriate values
+  let history = [];
+  const previousHistory = JSON.parse(localStorage.getItem("History")) || [];
+
+  history = [...previousHistory, { questionText, timeTaken, errorCount }];
+  localStorage.setItem("History", JSON.stringify(history));
+
+  displayHistory();
   display.classList.add("inactive");
   display.innerHTML = "";
   errorCount = 0;
@@ -97,6 +110,24 @@ startBtn.onclick = () => {
   }, 1000);
 };
 
+const displayHistory = () => {
+  let history = JSON.parse(localStorage.getItem("History"));
+  histories.innerHTML = "";
+  const row = history.forEach((element) => {
+    let div = document.createElement("div");
+    div.classList.add("card");
+    div.innerHTML = `
+    ${element.questionText}<br>
+    you took ${element.timeTaken} seconds. <br>
+    Mistake: ${element.errorCount}
+    `;
+
+    histories.appendChild(div);
+  });
+};
+
+displayHistory();
+
 const closeModal = () => {
   modal.classList.add("hidden");
   result.classList.add("hidden");
@@ -108,11 +139,17 @@ const validateKey = (key) => {
   }
   return false;
 };
-//show time
-setInterval(() => {
-  const currentTime = Date.now();
-  let time = (currentTime - startTime) / 1000;
-  time = Math.floor(time);
-  console.log(startTime);
-  showTime.innerHTML = `${startTime ? Math.floor(time) : 0} seconds`;
-}, 1000);
+
+// let history = [];
+// const previousHistory = JSON.parse(localStorage.getItem("History")) | [];
+
+// history = [...previousHistory, { text, time, error }];
+// localStorage.setItem("History", JSON.stringify(history));
+
+// //show time
+// setInterval(() => {
+//   const currentTime = Date.now();
+//   let time = (currentTime - startTime) / 1000;
+//   time = Math.floor(time);
+//   showTime.innerHTML = `${startTime ? Math.floor(time) : 0} seconds`;
+// }, 1000);
